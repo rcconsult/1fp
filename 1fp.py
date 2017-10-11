06e83ef6c852b08813fb503481439e57c722f7ee
tomas <tomastus01@gmail.com>
2017-10-11 20:48:18 +0200

    Added PYTTSX3

--- 1fp.py
+++ 1fp.py
@@ -1,1 +1,9 @@
 import os
+import pyttsx3
+import pygame
+import pyglet
+import OpenGL
+
+engine = pyttsx3.engine.Engine()
+engine.say("Hello, I am pyttsx3 and I love speaking")
+engine.runAndWait()
