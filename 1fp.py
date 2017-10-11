6307033c39e2c856266cd2ec963d1a5d90f46d47
tomas <tomastus01@gmail.com>
2017-10-11 20:59:36 +0200

    Added PYTTSX3

--- 1fp.py
+++ 1fp.py
@@ -5,5 +5,14 @@ import pyglet
 import OpenGL
 
 engine = pyttsx3.engine.Engine()
-engine.say("Hello, I am pyttsx3 and I love speaking")
+engine.say("Oh a burger")
+engine.say("IIII usedddd toooo bee an acow")
+engine.say("Ooooooooooooooohhhh")
+engine.say("And by the way, I hate eating acow burgers")
+engine.say("Hello I am Purple Sheep and today I will eat a cheese buger")
+engine.say("I am that weird burger acow thing and that weird man that says OOOOOOOh or 00000000000004444444")
+engine.say("Goodbyeeeeeee Chicken toaster heads!!!!")
+engine.say("And by the way I am scanning the computer for veruses")
+engine.say("After that I am going to launch a Minecraft either remake or rip off")
+engine.say("And I am going to be in game yeeeeeyyyyeeee!!!!????")
 engine.runAndWait()
